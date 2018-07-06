964ee7156e62dae4056df7993559500d38a1d827
hschallh <hschallh@gmail.com>
2018-07-06 12:23:37 -0400

    adventure bug: Golds do not count as treasure

--- projects/schallhh/dominion/dominion.c
+++ projects/schallhh/dominion/dominion.c
@@ -650,7 +650,7 @@ int adventurerEffect(int currentPlayer, struct gameState* state, int drawntreasu
         }
         drawCard(currentPlayer, state);
         cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer] - 1]; //top card of hand is most recently drawn card.
-        if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
+        if (cardDrawn == copper || cardDrawn == silver /*|| cardDrawn == gold*/)
             drawntreasure++;
         else {
             temphand[z] = cardDrawn;
