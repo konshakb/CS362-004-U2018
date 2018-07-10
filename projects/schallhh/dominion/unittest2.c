460bc49ee6574f18e21f16b41b1cf07550bde668
hschallh <hschallh@gmail.com>
2018-07-10 07:58:41 -0400

    add custom assert to handle logging for unit test 2

--- projects/schallhh/dominion/unittest2.c
+++ projects/schallhh/dominion/unittest2.c
@@ -5,15 +5,27 @@
 #include "dominion.h"
 #include "dominion_helpers.h"
 #include "rngs.h"
-#include <assert.h>
 #include <stdio.h>
 #include <string.h>
+
+int testsPassed = 1;
+
+void testAssert(int test, char* msg)
+{
+    if (!test) {
+        printf("TEST FAILED: %s\n", msg);
+        testsPassed = 0;
+    } else {
+#ifdef DEBUG_TEST
+        printf("TEST PASSED: %s\n", msg);
+#endif
+    }
+}
 
 int main()
 {
     int seed = 1000;
     int numPlayer = 2;
-
     int p, a, c, f;
     int maxProvince, maxAdventurer, maxCouncil, maxFeast;
     int k[10] = { adventurer, council_room, feast, gardens, mine, remodel, smithy, village, baron, great_hall };
@@ -21,8 +33,8 @@ int main()
 
     printf("Testing: isGameOver()\n");
 
-    memset(&G, 23, sizeof(struct gameState)); // clear the game state
-    initializeGame(numPlayer, k, seed, &G); // initialize a new game
+    memset(&G, 23, sizeof(struct gameState));
+    initializeGame(numPlayer, k, seed, &G);
 
     maxProvince = G.supplyCount[province];
     maxAdventurer = G.supplyCount[adventurer];
@@ -34,37 +46,23 @@ int main()
             for (c = 0; c <= maxCouncil; c++) {
                 for (f = 0; f <= maxFeast; f++) {
 
-#ifdef DEBUG_TESTS
-                    printf("Pile count:\n - Province: %d\n - Adventure: %d\n - Council Room: %d\n - Feast: %d\n - All Others: MAX\n", p, a, c, f);
-#endif
-
                     G.supplyCount[province] = p;
                     G.supplyCount[adventurer] = a;
                     G.supplyCount[council_room] = c;
                     G.supplyCount[feast] = f;
 
                     if (p == 0) {
-#ifdef DEBUG_TESTS
-                        printf("TEST: isGameOver() = %d, expected = %d\n", isGameOver(&G), 1);
-#endif
-                        assert(isGameOver(&G) == 1);
+                        testAssert(isGameOver(&G) == 1, "Game over when province pile depleated");
                     } else if (a == 0 && c == 0 && f == 0) {
-#ifdef DEBUG_TESTS
-                        printf("TEST: isGameOver() = %d, expected = %d\n", isGameOver(&G), 1);
-#endif
-                        assert(isGameOver(&G) == 1);
+                        testAssert(isGameOver(&G) == 1, "Game over when 3 piles depleated");
                     } else {
-#ifdef DEBUG_TESTS
-                        printf("TEST: isGameOver() = %d, expected = %d\n", isGameOver(&G), 0);
-#endif
-                        assert(isGameOver(&G) == 0);
+                        testAssert(isGameOver(&G) == 0, "Game not over when 2 or fewer piles depleated while province pile has one or more cards");
                     }
                 }
             }
         }
     }
 
-    printf("All tests passed!\n");
-
+    printf("%s\n", testsPassed ? "All tests passed" : "1 or more tests failed");
     return 0;
 }
