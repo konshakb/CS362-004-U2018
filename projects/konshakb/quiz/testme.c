7eeaa8f41d7cd1bb98899515816d316fda521d17
Bryan Konshak <bkonshak@gmail.com>
2018-07-23 02:20:15 -0400

    quiz added with makefile

---
+++ projects/konshakb/quiz/testme.c
@@ -0,0 +1,94 @@
+//Quiz Bryan Konshak
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+
+char inputChar()
+{
+    // TODO: rewrite this function
+    char randChar=' ';
+    int randNum=0;
+    randNum=rand()%100;
+    randNum=randNum+32;//lowercase
+    if(randNum<40)
+        randNum=32;
+    else if(randNum<91 && randNum!=40)
+        randNum=41;
+    if(randNum>101 && randNum<114)
+        randNum=101;
+    
+    randChar = (char) randNum;
+//    printf("%c\n", randChar);
+
+    return randChar;
+}
+
+char *inputString()
+{
+    char string[6];
+    char *str;
+    int i;
+    int randNum;
+    randNum=randNum+97;//lowercase
+    for (i=0;i<5;i++)
+    {
+        randNum=rand()%26;
+        randNum=randNum+97;//lowercase
+        if(randNum<101)
+            randNum=101;
+        if(randNum>116)
+            randNum=116;
+
+        string[i]=randNum;
+    }
+    string[5]='\0';
+    str=string;
+   // printf("%s\n", str);
+
+    return str;
+}
+
+void testme()
+{
+  int tcCount = 0;
+  char *s;
+  char c;
+  int state = 0;
+    c = inputChar();
+    s = inputString();
+  while (1)
+  {
+    tcCount++;
+    c = inputChar();
+    s = inputString();
+    printf("Iteration %d: c = %c, s = %s, state = %d\n", tcCount, c, s, state);
+
+    if (c == '[' && state == 0) state = 1;
+    if (c == '(' && state == 1) state = 2;
+    if (c == '{' && state == 2) state = 3;
+    if (c == ' '&& state == 3) state = 4;
+    if (c == 'a' && state == 4) state = 5;
+    if (c == 'x' && state == 5) state = 6;
+    if (c == '}' && state == 6) state = 7;
+    if (c == ')' && state == 7) state = 8;
+    if (c == ']' && state == 8) state = 9;
+    if (s[0] == 'r' && s[1] == 'e'
+       && s[2] == 's' && s[3] == 'e'
+       && s[4] == 't' && s[5] == '\0'
+       && state == 9)
+    {
+      printf("error ");
+      exit(200);
+    }
+  }
+}
+
+
+int main(int argc, char *argv[])
+{
+    srand(time(NULL));
+    testme();
+    return 0;
+}
